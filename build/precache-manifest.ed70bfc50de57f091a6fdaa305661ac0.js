49d7f55678a6ba67b4b05d9135c2e306d59faef3
Artur Gunka <arturel101010@mail.ru>
2020-07-09 14:17:12 +0300

    build for githubPages

---
+++ build/precache-manifest.ed70bfc50de57f091a6fdaa305661ac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0ed65190642984de7d896d2fc98260",
+    "url": "/posts-redux-saga/index.html"
+  },
+  {
+    "revision": "86ba565c6932fd5de7f4",
+    "url": "/posts-redux-saga/static/js/2.a24a3294.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/posts-redux-saga/static/js/2.a24a3294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4859a79d0b078555012",
+    "url": "/posts-redux-saga/static/js/main.c94759e7.chunk.js"
+  },
+  {
+    "revision": "6cd7b3f617b29932a6c9",
+    "url": "/posts-redux-saga/static/js/runtime-main.6949580b.js"
+  }
+]);
